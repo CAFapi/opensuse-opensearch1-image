0f529f28c1bb553347be6c549ec40a3ef2aa5f71
vgkusuma <vgkusuma@microfocus.com>
2022-05-25 14:26:16 -0700

    Cleanup test

--- src/test/java/cafapi/opensuse/opensearch1/ContainerIT.java
+++ src/test/java/cafapi/opensuse/opensearch1/ContainerIT.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.apache.http.HttpHost;
-import org.apache.http.ParseException;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -36,14 +35,11 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.util.EntityUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
-import org.opensearch.client.Request;
-import org.opensearch.client.Response;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.json.jackson.JacksonJsonpMapper;
@@ -57,24 +53,10 @@ import org.opensearch.client.opensearch.indices.IndexSettings;
 import org.opensearch.client.transport.OpenSearchTransport;
 import org.opensearch.client.transport.rest_client.RestClientTransport;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public final class ContainerIT {
 
-    private final static ObjectMapper objectMapper = new ObjectMapper();
     private static final int CONNECT_TIMEOUT = 60000;
     private static final int SOCKET_TIMEOUT = 60000;
-
-    private final RestClient restClient;
-    private final OpenSearchTransport transport;
-    private final OpenSearchClient client;
-
-    public ContainerIT() {
-        restClient = getOpenSearchRestClient();
-        transport = getOpenSearchTransport();
-        client = new OpenSearchClient(transport);
-    }
 
     @Rule
     public TestRule watcher = new TestWatcher() {
@@ -86,7 +68,9 @@ public final class ContainerIT {
 
     @Test
     public void testIndexCreation() throws IOException {
-        try {
+        try (final RestClient restClient = getOpenSearchRestClient();
+            final OpenSearchTransport transport = getOpenSearchTransport(restClient);) {
+            final OpenSearchClient client = new OpenSearchClient(transport);
             System.out.println("Got OpenSearchClient :" + client);
 
             final HealthResponse response = client.cluster().health();
@@ -116,84 +100,10 @@ public final class ContainerIT {
 
             assertTrue("Index response was not acknowledged", createIndexResponse.acknowledged());
             assertTrue("All shards were not copied", createIndexResponse.shardsAcknowledged());
-        } catch (final Exception e) {
-            e.printStackTrace();
-            fail("testIndexCreation failed" + e);
-        } finally {
-            restClient.close();
         }
     }
 
-    // @Test
-    public void testClusterHealth() throws IOException {
-        try {
-            System.out.println("Got OpenSearchClient :" + client);
-
-            final HealthResponse response = client.cluster().health();
-            System.out.println("Got HealthResponse :" + response);
-            final HealthStatus status = response.status();
-            System.out.println("Got HealthStatus :" + status.jsonValue());
-            assertEquals("Elasticsearch status not green", HealthStatus.Green, status);
-        } catch (final Exception e) {
-            e.printStackTrace();
-            fail("testClusterHealth failed" + e);
-        } finally {
-            restClient.close();
-        }
-    }
-
-    // @Test
-    public void testClusterStatus() throws IOException {
-        final Response response;
-        try {
-            System.out.println("Executing ES cluster health check...");
-            // Exclude system indices from healthcheck
-            final String endPoint = "_cluster/health/*,-.*";
-            final Request healthRequest = new Request("GET", endPoint);
-            healthRequest.addParameter("wait_for_status", "yellow");
-            response = restClient.performRequest(healthRequest);
-        } catch (final IOException ex) {
-            System.out.println("Error executing cluster health check request." + ex);
-            ex.printStackTrace();
-            fail("Error executing cluster health check request.");
-            return;
-        }
-
-        final String healthResponse;
-        try {
-            healthResponse = EntityUtils.toString(response.getEntity());
-        } catch (final ParseException | IOException ex) {
-            System.out.println("Error parsing cluster health check Response" + ex);
-            fail("Error parsing cluster health check Response.");
-            return;
-        }
-
-        System.out.println("ClusterHealthResponse : " + healthResponse);
-
-        if (healthResponse == null) {
-            System.out.println("No HealthCheck response from ElasticSearch");
-            fail("No HealthCheck response from ElasticSearch");
-            return;
-        }
-
-        final String status;
-        try {
-            status = objectMapper.readTree(healthResponse).get("status").asText();
-        } catch (JsonProcessingException ex) {
-            System.out.println("HealthCheck response could not be processed" + ex);
-            fail("HealthCheck response could not be processed");
-            return;
-        }
-
-        System.out.println("Got ES status : " + status);
-        if (status.equals("red")) {
-            System.out.println("Elasticsearch is unhealthy.");
-            fail("Elastic search status invalid: " + status);
-        }
-        assertEquals("Elasticsearch status not green", "green", status);
-    }
-
-    private OpenSearchTransport getOpenSearchTransport() {
+    private OpenSearchTransport getOpenSearchTransport(final RestClient restClient) {
         System.out.println("Creating OpenSearchTransport...");
         final OpenSearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         return transport;
